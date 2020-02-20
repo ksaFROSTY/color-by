b95a8793932efab8b47ae6f9296fd0b3a86ea7a3
ksaFROSTY <59263338+ksaFROSTY@users.noreply.github.com>
2020-02-20 20:51:50 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({disableEveryone: true, maxMessagesCache: 1});
+const convert = require("hh-mm-ss")
+const dateFormat = require('dateformat');
+const fs = require('fs');
+const pretty = require('pretty-ms');
+const rn = require('random-number');
+const moment = require('moment');
+var Canvas = require('canvas')
+var jimp = require('jimp')
+const ms = require("ms");
+let done = {};
+const prefix = '!'
+
+           
+            
+           
+
+
+
+
+
+
+
+
+//By FROSTY
+
+
+
+       
+
+
+
+//token
+client.login("NjcyODM5ODU0MjIwMTE1OTY4.XjbRvA.jWtGv4mICiaF9xAwz0FeDhMV0v4")
